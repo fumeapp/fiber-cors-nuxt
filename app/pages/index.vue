38dae805845862a23734c3f1e0e4498d30cda1ad
kevin olson <acidjazz@gmail.com>
2025-06-03 19:03:52 -0500

    🎨 touchups

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,60 +1,19 @@
 <script lang="ts" setup>
-
-const headers = ref(undefined)
-
-const cors = () => $fetch('https://d245hvitoez60u.cloudfront.net/', {
-  method: 'OPTIONS',
-  onResponse({ response }) {
-    console.log(request, response, options)
-  },
-})
-
 const { data, refresh, status, error } = useFetch('https://d245hvitoez60u.cloudfront.net/')
-
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 h-screen max-w-xl w-full mx-auto">
+  <div class="flex flex-col items-center justify-center gap-4 h-screen max-w-4xl w-full mx-auto">
     <h1 class="font-bold text-2xl text-(--ui-primary)">
       fiber CORS test
     </h1>
 
     <div class="flex items-center gap-2">
       <UButton
-        label="Refresh CORS"
-        @click="cors"
-      />
-
-      <UButton
         label="Refresh GET"
         @click="refresh"
       />
-
-
-      <UButton
-        label="GitHub"
-        color="neutral"
-        variant="outline"
-        icon="i-simple-icons-github"
-        to="https://github.com/nuxt/ui"
-        target="_blank"
-      />
-    </div>
-
-    <UCard class="w-full">
-      <template #header>
-        <div class="flex justify-between">
-
-        <UBadge color="neutral"> OPTIONS https://d245hvitoez60u.cloudfront.net/ </UBadge>
-        <UButtonGroup>
-          <UBadge>status</UBadge>
-          <UBadge>{{  status }}</UBadge>
-        </UButtonGroup>
-        </div>
-      </template>
-      <pre> {{ headers }}</pre>
-    </UCard>
- 
+   </div>
 
     <UCard class="w-full">
       <template #header>
@@ -67,8 +26,7 @@ const { data, refresh, status, error } = useFetch('https://d245hvitoez60u.cloudf
         </UButtonGroup>
         </div>
       </template>
-      <pre> {{ data }}</pre>
-      <template #footer> {{ headers }}</template>
+      <pre class="text-xs overflow-scroll"> {{ data }}</pre>
     </UCard>
     <UAlert v-if="error" color="error" variant="subtle">
       <template #description>
